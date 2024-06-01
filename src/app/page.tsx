7228929d04e0ace53a5db7c586294f45c043a494
Ethan Pelletier <ethan@gradison.ca>
2024-06-01 17:28:44 -0300

    add text animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 // import IconCloud from "@/components/magic/icon-cloud";
 import TextRevealByWord from "@/components/magic/text-reveal";
+import TypingAnimation from "@/components/magic/typing-animation";
 import WavyText from "@/components/magic/wavy-text";
 import WordRotate from "@/components/magic/word-rotate";
 // import { Button } from "@/components/ui/button";
@@ -22,7 +23,7 @@ export default function Home() {
         <div id="nav" className="flex justify-between py-4 px-8 items-center">
           <div>
             {/* Icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="rgba(255, 0, 0, 0.6)" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-handshake hover:scale-110 hover:-mt-2 hover:rotate-12 transition-all duration-200"><path d="m11 17 2 2a1 1 0 1 0 3-3" /><path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4" /><path d="m21 3 1 11h-2" /><path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3" /><path d="M3 4h8" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="rgba(255, 0, 0, 0.3)" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-handshake hover:scale-110 hover:-mt-2 hover:rotate-12 transition-all duration-200"><path d="m11 17 2 2a1 1 0 1 0 3-3" /><path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4" /><path d="m21 3 1 11h-2" /><path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3" /><path d="M3 4h8" /></svg>
             {/* <Image
             src={"/logo.svg"}
             alt="BlM Logo"
@@ -86,26 +87,36 @@ export default function Home() {
 
       </div>
       <section id="history">
-      <div className="px-12 py-12 overflow-hidden flex flex-col justify-start items-start">
-        <div className="flex flex-col gap-y-3 w-full">
-          <span className="text-5xl text-white">
-            History
-          </span>
-          <hr className="border border-white/20 w-full" />
+        <div className="px-12 py-12 overflow-hidden flex flex-col justify-start items-start">
+          <div className="flex flex-col gap-y-3 w-full">
+            <TypingAnimation text="History" className="text-5xl text-start my-4 text-white"/>
+
+            <hr className="border border-white/20 w-full" />
+          </div>
+
+          {/* Introduction to the history of racism */}
+
+          <TypingAnimation text="The history of racism is deeply rooted in the fabric of societies around the world. It has shaped and been shaped by social, economic, and political forces, leading to systemic inequalities that persist today. Understanding this history is crucial to addressing and dismantling racism in all its forms." className="mt-6 text-white text-start text-lg" duration={5}/>
+
+
+          {/* Historical Events and Figures */}
+          <div className="mt-6 text-white">
+            <h2 className="text-3xl">Historical Events and Figures</h2>
+          </div>
+          <TypingAnimation text="Significant events such as the Civil Rights Movement in the United States, the abolition of apartheid in South Africa, and the ongoing struggles of Indigenous peoples globally highlight the fight against racial injustice. Key figures like Martin Luther King Jr., Nelson Mandela, and Malala Yousafzai have played pivotal roles in advocating for equality and human rights." className="mt-6 text-white text-start text-lg" duration={5}/>
+
+          {/* Role of education and awareness */}
+          <div className="mt-6 text-white">
+            <h2 className="text-3xl">Role of Education and Awareness</h2>
+          </div>
+          <TypingAnimation text="Education and awareness are fundamental in combating racism. By learning about the history and impact of racism, individuals and communities can better understand the importance of diversity and inclusion. Promoting open dialogues and educational programs can foster a more inclusive and equitable society." className="mt-6 text-white text-start text-lg" duration={5}/>
         </div>
-
-        {/* Introduction to the history of racism */}
-        
-        {/* Historical Events and Figures */}
-
-        {/* Role of education and awareness */}
-      </div>
       </section>
 
 
       <div className="px-12 overflow-hidden flex flex-col justify-center items-center gap-y-4">
         <span className="text-xl text-white">
-        STAND UP FOR WHAT IS RIGHT
+          STAND UP FOR WHAT IS RIGHT
         </span>
         <hr className="border border-white/20 w-[100px]" />
 
@@ -114,6 +125,16 @@ export default function Home() {
       </div>
 
       {/* Call to action */}
+      <section id="call-to-action" className="px-12 py-12 mt-12 flex flex-col justify-center items-center bg-red-950/30 text-white">
+        {/* <h2 className="text-4xl mb-4">Join the Movement</h2> */}
+        <TypingAnimation text="Join the Movement" className="text-5xl my-4"/>
+        <p className="text-xl mb-6 text-center">
+          Take a stand against racism. Get involved in your community, educate yourself and others, and advocate for policies that promote equality and justice. Together, we can create a world where everyone is treated with respect and dignity.
+        </p>
+        <button className="bg-transparent border border-white text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-white hover:text-black hover:scale-105 transition-all duration-300 ">
+          Get Involved
+        </button>
+      </section>
 
     </>
   );
